83ac0143b3b80fc8c0b70741a9c0c6f2da67d631
Jean-Louis Boudrand <jeanlouis.boudrand@hotmail.fr>
2015-11-22 02:58:24 +0100

    Improve half to float (thanks Chris for the patch)

--- W2ENT_QT/CW3ENTMeshFileLoader.cpp
+++ W2ENT_QT/CW3ENTMeshFileLoader.cpp
@@ -13,6 +13,7 @@
 #include "IFileSystem.h"
 #include "IReadFile.h"
 #include "IWriteFile.h"
+#include "halffloat.h"
 
 #include <sstream>
 #include <iostream>
@@ -65,37 +66,6 @@ bool CW3ENTMeshFileLoader::isALoadableFileExtension(const io::path& filename) co
 
     file->drop();
     return false;
-}
-
-
-// Code from http://download.autodesk.com/us/maya/2009help/API/dds_float_reader_8cpp-example.html
-const double two_pow_neg14 = pow(2.0, -14.0);
-inline float halfToFloat(unsigned short val)
-{
-        float outValue = 0.0f;
-
-
-#if defined(OSMac_)
-        // Need to swap bytes on Power PC (Mac)
-        swap_endian_half( &val );
-#endif
-
-        // Convert 16-value into a float...
-        double h_mantissa = (float) (val & 1023); // Mantissa = low order 10  bits
-        double h_exponent = (float)  ((val >> 10) & 31); // Exponent = next 5 bits
-        unsigned int i_sign = (val >> 15) & 1;  ; // Sign is the highest bit
-        double h_sign = (i_sign == 0) ? 1.0 : -1.0;
-
-        if (h_exponent != 30.0)
-        {
-                outValue = (float) (h_sign * pow(2.0, h_exponent-15.0) * ( 1.0 + ( h_mantissa / 1024.0 )));
-        }
-        else
-        {
-                outValue = (float) ( h_sign * pow(2.0, two_pow_neg14) * ( h_mantissa / 1024.0 ) );
-        }
-        //printf("[%d][%d] = inValue=%d, isign =%d, sign=%g, exp=%g, mant=%g, value=%g\n", y,x, *inPtr, i_sign, h_sign, h_exponent, h_mantissa, outValue);
-        return outValue;
 }
 
 

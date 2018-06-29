7e0ea09f5141c45e60f2f25490d924f56f3f8b1a
Jean-Louis Boudrand <jeanlouis.boudrand@hotmail.fr>
2018-06-30 01:17:28 +0200

    Update packager

--- packager/packager.bat
+++ packager/packager.bat
@@ -1,35 +1,46 @@
-set release-dir="../../Dev/build-W2ENT_QT-MinGW_32bit/release"
-set data-dir="../data"
+cd %~dp0
+
+set release-dir=./../../Dev/build-W2ENT_QT-MinGW_32bit/release
+set data-dir=../data
+set tools-dir=../tools
 set qt-plugin-dir=C:\Qt\5.11.1\mingw53_32\plugins
 
-set /p version=Version:
 set prefix=The_witcher_converter
+set /p version=Version(%prefix%_version):
 
+set foldername=./%prefix%_%version%
 set filename=%prefix%_%version%.zip
 echo %filename%
 
+
+:: create folder
+rmdir "%foldername%"
+mkdir "%foldername%"
+
 ::copy qwindows.dll
-rmdir plugins
-mkdir plugins
-copy "%qt-plugin-dir%\platforms\qwindows.dll" ".\plugins"
+set foldername-plugin=%foldername%/plugins
+mkdir "%foldername-plugin%"
+copy "%qt-plugin-dir%\platforms\qwindows.dll" "%foldername-plugin%"
 
-set filelist=%release-dir%/The_Witcher_Converter.exe
-set filelist= %filelist% %release-dir%/Qt5Core.dll
-set filelist= %filelist% %release-dir%/Qt5Gui.dll
-set filelist= %filelist% %release-dir%/Qt5Widgets.dll
-set filelist= %filelist% %release-dir%/Qt5Xml.dll
-set filelist= %filelist% %release-dir%/libgcc_s_dw2-1.dll
-set filelist= %filelist% %release-dir%/libstdc++-6.dll
-set filelist= %filelist% %release-dir%/libwinpthread-1.dll
-set filelist= %filelist% %release-dir%/Irrlicht.dll
-set filelist= %filelist% %release-dir%/libassimp.dll
-set filelist= %filelist% %release-dir%/liblz4.dll
-set filelist= %filelist% %release-dir%/snappy32.dll
-set filelist= %filelist% %data-dir%
-set filelist= %filelist% plugins
-echo %filelist%
+::copy tools
+set foldername-tools=%foldername%/tools
+mkdir "%foldername-tools%"
+xcopy "%tools-dir%" "%foldername-tools%"
 
-set command=7z.exe a %filename% %filelist%
+:: copy dll and exe
+cd "%release-dir%"
+copy *.dll "%~dp0/%foldername%"
+copy The_Witcher_Converter.exe "%~dp0/%foldername%"
+cd %~dp0
+
+:: copy data folder
+cd "%data-dir%"
+copy * "%~dp0/%foldername%"
+mkdir "%~dp0%foldername%/langs"
+xcopy langs "%~dp0%foldername%/langs"
+cd %~dp0
+
+set command=7z.exe a %filename% %foldername%
 echo %command%
 %command%
 pause
